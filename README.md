c41f8f5deaaa1ac3b9d0f04cab68c86b67da5500
Aaron Brooks <aaronbrooks322@gmail.com>
2013-10-18 23:40:39 -0500

    Inital Commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Until Web App
+
+## Ruby version
+  2.0.0-p247
+
+## System dependencies
+  Postgresql and PG gem
+  Jquery
+
+## Configuration
+
+
+## Database creation
+
+
+## Database initialization
+
+
+## How to run the test suite
+
+
+## Services (job queues, cache servers, search engines, etc.)
+
+
+## Deployment instructions
+
+
+## ...
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Until::Application.config.secret_key_base = '4bde015cd8329b84de1986b10fdae757e3273e4d1b5ff476449df765fa8975c26c08161920a96a849f236a6b10694c4a5b7fe4d60233c48d15ce4710f4fc717f'
